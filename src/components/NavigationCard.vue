3d9bf18f70117ae9ce1eaadce86ab19e070debd8
kengork <saracuriel.p@gmail.com>
2019-06-04 12:27:41 +0200

    feat(ui-compo): implement top-bar component

---
+++ src/components/NavigationCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="navigation">
+    <BaseCard :title="title" class="navigation shadow">
+      <p class="content">
+        <slot></slot>
+      </p>
+      <div class="button-group">
+      <Button type="navigation-button" @click="previousStep">Previous Step</Button>
+      <Button type="navigation-button" @click="nextStep">Next Step</Button>
+      </div>
+    </BaseCard>
+  </div>
+</template>
+
+<script>
+import Button from '@/components/Button'
+import BaseCard from '@/components/BaseCard'
+
+export default {
+  name: 'NavigationCard',
+  props: {
+    title: String,
+    previousStep: Function,
+    nextStep: Function,
+  },
+  components: {
+    Button,
+    BaseCard,
+  },
+  methods: {
+    handleKeyUp: event => {
+      if (event.keyCode === 13) {
+        this.props.nextStep()
+      }
+    },
+  },
+}
+</script>
+
+<style lang='scss'>
+@import '@/styles/app.global.scss';
+@import '@/styles/_colors.scss';
+
+.navigation > .title {
+  background: $sheikah-gradient;
+  color: $grey-2;
+  font-size: $loading-modal-title-font-size;
+  font-weight: 100;
+}
+
+.shadow {
+  box-shadow: $default-box-shadow;
+}
+
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="top-bar">
+      <div class="tab">
+        <router-link to="/wallet/transaction">Transaction</router-link>
+      </div>
+      <div class="tab">
+        <router-link to="/wallet/receive">Receive</router-link>
+      </div>
+      <div class="tab">
+        <router-link to="/wallet/send">Send</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '@/styles/_colors.scss';
+@import '@/styles/theme.scss';
+
+.top-bar {
+  border-bottom: $top_bar-border;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  padding: 13px;
+  .tab {
+    align-items: center;
+    border-bottom: $top_bar-link-inactive-border;
+    font-weight: bold;
+    text-decoration: none;
+    padding: 15px 8px;
+  }
+  .tab a {
+    padding: 24px;
+    align-items: center;
+    border-bottom: $top_bar-link-inactive-border;
+    color: $sidebar-link-color;
+    font-weight: bold;
+    text-decoration: none;
+    &.router-link-active{
+      border-bottom: 3px solid $blue-6;
+      color: $sidebar-active-color;
+    }
+  }
+}
+</style>
