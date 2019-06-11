196a48ee7993314922b1665d68b4737a3296237b
kengork <saracuriel.p@gmail.com>
2019-06-11 14:55:28 +0200

    feat(ui-compo): wallet seed validation + input big

---
+++ src/components/InputBig.vue
@@ -0,0 +1,38 @@
+<template>
+  <textarea
+    class="big"
+    :value="value"
+    :onChange="onChange"
+  />
+</template>
+<script>
+export default {
+  name: 'InputBig',
+  props: {
+    value: String,
+    onChange: Function
+  }
+}
+</script>
+<style lang="scss">
+@import '@/styles/theme.scss';
+
+.big {
+  border: $input_big-border;
+  border-radius: $input_big-border-radius;
+  min-height: 80px;
+  transition: all 0.1s ease;
+  width: $input_big-width;
+
+  &:hover,
+  &:focus {
+    border: $input_big-hover-border;
+    box-shadow: $input_big-hover-box-shadow;
+    outline: 0;
+    outline-color: transparent;
+    outline-style: none;
+  }
+}
+</style>
+
+
---
+++ src/components/steps/WalletSeedValidation.vue
@@ -0,0 +1,61 @@
+<template>
+  <NavigationCard class="wallet-seed-validation" title="Confirm your seed phrase" previousText="Back" nextText="Next">
+    <!-- {this.props.paragraphs.map((text, i) => <p key={i}>{text}</p>)} -->
+    <p>"Please type your 12 word seed phrase exactly as it was shown to you on the previous " +
+      "screen.",
+      "This step is to confirm that you have copied your seed phrase correctly.",</p>
+    <InputBig 
+      class="seed"
+      :value="inputValue"
+      :onChange="onChangeInput" 
+    />
+    <p>
+      Please ensure you do not add any extra spaces between words or at the beginning or end
+      of the phrase.
+    </p>
+  </NavigationCard>
+</template>
+
+<script>
+import NavigationCard from '@/components/NavigationCard'
+export default {
+  name: 'WalletSeedValidation',
+  components: {
+    NavigationCard
+  },
+  props: {
+    value: String,
+    onChangeInput: String,
+    inputValue: String,
+  }
+}
+</script>
+<style lang="scss">
+@import '@/styles/theme.scss';
+
+.wallet-seed-validation .shadow {
+  height: 450px;
+}
+
+.wallet-seed-validation .shadow .content {
+  height: 290px;
+  padding: 30px;
+}
+
+.seed {
+  align-items: center;
+  border: $input_big-border;
+  border-radius: $input_big-border-radius;
+  box-sizing: border-box;
+  color: $input_big-color;
+  display: inline-flex;
+  font-size: 16px;
+  line-break: auto;
+  line-height: 1.5em;
+  margin: 15px 0;
+  padding: 20px 25px;
+  width: 100%;
+}
+</style>
+
+
