7077e7bd4c8b936dae777bf933dd57ea96cfb9c9
kengork <saracuriel.p@gmail.com>
2019-06-12 15:29:07 +0200

    fix(ui-compo): base card fixes

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -21,18 +21,15 @@ export default {
   @import '@/styles/theme.scss';
 
   .card {
-    max-width: 500px;
     border: 1px solid #EEE;
     border-radius: 2px;
-  }
-
-  .shadow {
     box-shadow: $default-box-shadow;
+    height: 550px;
+    max-width: 600px;
   }
 
   .title {
     border-bottom: solid 1px $grey-4;
-    padding: 20px;
   }
 
 </style>
